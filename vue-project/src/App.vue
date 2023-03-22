568ca0fc6a68d0a58a965bc6431fe122f4469554
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-03-22 10:15:49 -0400

    Colmmit 17

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,20 +4,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <h1>SHOP FOR FOOD</h1>
+  <h2>Select items to buy</h2>
 </template>
 
 <style scoped>
