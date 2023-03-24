0eec41b19b93e3c028b74ce885b8b16d28d3db3f
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-03-24 09:46:41 -0400

    save

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -10,4 +10,87 @@ export default{
 </script>
 <style>
 </style>
+<script></script>
+selected:"",
+menu: [
+{
+    name: "Caprese Pizza",
+    info: "Includes Mozzarella Cheese with fresh basil and fresh tomato",
+    price: 25,
+    img: "https://cdn.loveandlemons.com/wp-content/uploads/2019/09/margherita-pizza.jpg",
+    type: "pizza",
+},
+{
+    name: "Pepperoni Pizza",
+    info: "Includes fresh tomato sauce with Mozzarella Cheese and freshly made pepperoni",
+    price: 30,
+    img: "https://www.theimpulsivebuy.com/wordpress/wp-content/uploads/2022/01/phspicylover1.jpeg",
+    type: "pizza",
+},
+{
+    name: "Hawaiian Pizza",
+    info: "Includes pineapple with Mozzarella Cheese and bacon",
+    price: 30,
+    img: "https://images.thebrag.com/tb/uploads/2022/08/pizza-hut-hawaiian-pizza-768x494.jpg",
+    type: "pizza",
+},
+{
+    name: "Veggie Pizza",
+    info: "Includes Mozzarella Cheese, tomato sauce, bell peppers and olives",
+    price: 35,
+    img: "https://www.secondrecipe.com/wp-content/uploads/2021/10/air-fryer-pizza-1.jpg",
+    type: "pizza",
+},
+{
+    name: "Chicago Deep Dish Pizza",
+    info: "Includes freshly made tomato sauce with Mozzarella Cheese topped with fresh cut basil",
+    price: 40,
+    img: "https://giordanos.com/wp-content/uploads/Hero-image_1210-v2.jpg",
+    type: "pizza",
+},
+{
+    name: "Sushi Pizza",
+    info: "Includes sushi rice and salmon, tuna with mayo",
+    price: 45,
+    img: "https://i.pinimg.com/originals/2d/5b/e4/2d5be4e118530ae96d4f7c789776af77.png",
+    type: "pizza",
+},
+{
+    name: "Espresso",
+    info: "Caffeine",
+    price: 50,
+    img: "https://globalassets.starbucks.com/assets/ec519dd5642c41629194192cce582135.jpg?impolicy=1by1_wide_topcrop_630",
+    type: "drink",
+},
+{
+    name: "Strawberry Smoothie",
+    info: "Made with fresh strawberries",
+    price: 8,
+    img: "https://www.thefieryvegetarian.com/wp-content/uploads/2021/04/vegan-strawberry-banana-smoothe-735x1106.jpg",
+    type: "drink",
+},
+{
+    name: "Banana Smoothie",
+    info: "Made with Freshly cut bananas",
+    price: 6,
+    img: "https://nourishplate.com/wp-content/uploads/2021/06/Apple-Banana-Smoothie-Recipe-7.jpg",
+    type: "drink",
+},
+{
+    name: "Mixed Fruits",
+    info: "Freshly cut strawberries, peaches, oranges, and bananas",
+    price: 12,
+    img: "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Chilled-Mixed-Fruit_exps41090_CW143039C09_16_6bC_RMS.jpg",
+    type: "fruit",
+},
+{
+    name: "Mixed Berries",
+    info: "Freshly picked blackberries, blueberries, and rasberries",
+    price: 10,
+    img: "https://healthyfitnessmeals.com/wp-content/uploads/2018/01/berry-fruit-salad-5.jpg",
+    type: "fruit",
+},
+];
+
+
 
